1d1d483c6c4a4e01ae9fa7ecb0106f32ed94e761
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-08-27 13:38:08 +0600

    api XMLHttpRequest

--- api/XMLHttpRequest/main.js
+++ api/XMLHttpRequest/main.js
@@ -1,1 +1,29 @@
-alert('hello');
+makeRequest = (method, url, data) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = () => {
+        let data = xhr.response;
+        console.log(JSON.parse(data));
+    }
+    xhr.onerror = () => {
+        console.log('some thing is wrong')
+    }
+    xhr.setRequestHeader('Content-type', 'application/json',)
+    
+    xhr.send(JSON.stringify(data));
+}
+
+const getData = () => {
+    makeRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+}
+
+const sendData = () => {
+    makeRequest('POST', 'https://jsonplaceholder.typicode.com/posts', {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    });
+}
+
+// getData();
+sendData();
